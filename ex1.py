47a1a073372ed5bb192c41b0f6abd812fd84b69f
AaronL8 <aaron.lauang1@ucalgary.ca>
2025-02-05 16:19:01 -0700

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -4,13 +4,15 @@ Answer to Questions
 2. The function represents a divide-and-conquer algorithm as it uses recursion
 to combine the results of each function call when it finally reaches n = 0
 or n = 1, ultimately returning the n-th value of the Fibonacci sequence.
-3.O(n)
+3. O(2^n)
 5.
 '''
 
 import timeit
 import sys
 from functools import wraps
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Question 4
 def memoize(func):      # decorator
@@ -31,9 +33,6 @@ def fibonacci(n):
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
 
-tm = timeit.timeit(lambda: fibonacci(20), number=1)
-print(tm)
-
 # Question 6
 # fibonacci without memoization
 def orig_fibonacci(n):
@@ -42,11 +41,33 @@ def orig_fibonacci(n):
     else:
         return orig_fibonacci(n - 1) + orig_fibonacci(n - 2)
     
-tm = timeit.timeit(lambda: orig_fibonacci(20), number=1)
-print(tm)
+if __name__ == "__main__":
 
-no_memo = {}
-with_memo = {}
+    tm = timeit.timeit(lambda: fibonacci(20), number=1)
+    print(tm)
+    tm = timeit.timeit(lambda: orig_fibonacci(20), number=1)
+    print(tm)
 
-for i in range(36):
-    tm = timeit.timeit(lambda: orig_fibonacci())
+    no_memo = {}
+    with_memo = {}
+
+    for i in range(1, 35):
+        tm = timeit.timeit(lambda: orig_fibonacci(i), number=1)
+        no_memo[i] = tm
+
+    print(no_memo)
+
+    for i in range(1, 35):
+        tm = timeit.timeit(lambda: fibonacci(i), number=1)
+        with_memo[i] = tm
+
+    no_memo = np.array(no_memo)
+    print(no_memo)
+    with_memo = np.array(with_memo)
+
+    x_values = list(range(1, 35))
+    x_values = np.array(x_values)
+    print(x_values)
+    
+    plt.plot(x_values, no_memo)
+    plt.show()
